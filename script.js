2d6a47f48ecc8ad8361c9fa8ab681f0e3f2f4845
Stathis Kot <stathiskotanidhs@gmail.com>
2023-12-01 23:27:14 +0200

    final

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const currentScoreTwo = document.querySelector('#current--1');
 const currentImage = document.querySelector('.dice');
 const rollDiceBtn = document.querySelector('.btn--roll');
 const holdBtn = document.querySelector('.btn--hold');
+const newGame = document.querySelector('.btn--new')
 
 overallScoreOne.textContent = '0';
 overallScoreTwo.textContent = '0';
@@ -16,36 +17,61 @@ let currentScore = 0;
 let activePlayer = 0;
 let totalScoreOne = 0;
 let totalScoreTwo = 0;
+let playing = true;
 
 function rollDice() {
   let dice = Math.floor(Math.random() * 6) + 1;
   currentImage.style.display = 'block';
-  currentImage.src = `dice-${dice}.png`;
+  currentImage.src = `images/dice-${dice}.png`;
   return dice;
 }
 
 rollDiceBtn.addEventListener('click', () => {
-  let rolledDice = rollDice();
-  if (rolledDice === 1) {
-    //switch player
-    document.querySelector(`#current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-  } else {
-    currentScore += rolledDice;
-    document.querySelector(`#current--${activePlayer}`).textContent =
-      currentScore; // dynamically!
+  if (playing) {
+    let rolledDice = rollDice();
+    if (rolledDice === 1) {
+      //switch player
+      document.querySelector(`#current--${activePlayer}`).textContent = 0;
+      currentScore = 0;
+      activePlayer = activePlayer === 0 ? 1 : 0;
+    } else {
+      currentScore += rolledDice;
+      document.querySelector(`#current--${activePlayer}`).textContent =
+        currentScore; // dynamically!
+    }
   }
+
 });
 
 holdBtn.addEventListener('click', () => {
-  if (activePlayer === 0) {
-    totalScoreOne += currentScore;
-    overallScoreOne.textContent = totalScoreOne;
-  } else {
-    totalScoreTwo += currentScore;
-    overallScoreTwo.textContent = totalScoreTwo;
+  if (playing) {
+    if (activePlayer === 0) {
+      totalScoreOne += currentScore;
+      overallScoreOne.textContent = totalScoreOne;
+    } else {
+      totalScoreTwo += currentScore;
+      overallScoreTwo.textContent = totalScoreTwo;
+    }
+    currentScore = 0;
+    if (totalScoreOne >= 20) {
+      overallScoreOne.textContent = 'Player 1 won'
+      playing = false;
+    } else if (totalScoreTwo >= 20) {
+      overallScoreTwo.textContent = 'Player 2 won'
+      playing = false;
+    }
+    activePlayer = activePlayer === 0 ? 1 : 0;
   }
-  currentScore = 0;
-  activePlayer = activePlayer === 0 ? 1 : 0;
 });
+
+newGame.addEventListener('click', () => {
+
+  overallScoreOne.textContent = 0;
+  overallScoreTwo.textContent = 0;
+  currentImage.style.display = 'none';
+  currentScoreOne.textContent = 0
+  currentScoreTwo.textContent = 0
+  currentScore = 0;
+  activePlayer = 0;
+
+})
